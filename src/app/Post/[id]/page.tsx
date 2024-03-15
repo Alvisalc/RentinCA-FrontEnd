a615612e10c7c6c1521e4d09c6f49868b5f8fde3
Alvisalc <alvisccy1107@gmail.com>
2024-03-14 22:51:52 -0400

    app

--- src/app/Post/[id]/page.tsx
+++ src/app/Post/[id]/page.tsx
@@ -1,21 +1,18 @@
-import { useRouter } from "next/navigation"; // Ensure correct import from next/navigation
+// import { useRouter } from "next/navigation"; // Ensure correct import from next/navigation
+
+"use client"
 import React, { useEffect, useState } from "react";
 import { supabasePublic } from "@/utils/supabase";
 import { RentPostData } from "@/types/data";
 
 const PostDetails = () => {
   const [post, setPost] = useState<RentPostData | null>(null);
-  const router = useRouter();
+  // const router = useRouter();
 
   useEffect(() => {
     const fetchPost = async () => {
-      const postId = router.query.id as string; // Directly attempt to access the dynamic route parameter
-
-      if (!postId) {
-        console.log("Post ID not available yet.");
-        return; // Optionally, handle this case more gracefully
-      }
-
+      const searchParams = new URLSearchParams(window.location.search);
+      const postId = searchParams.get('id');
       const { data, error } = await supabasePublic
         .from("RentPost")
         .select("*")
@@ -30,13 +27,14 @@ const PostDetails = () => {
     };
 
     fetchPost();
-  }, [router.query.id]); // Depend directly on the relevant part of the router state
+
+  }, []); // Depend directly on the relevant part of the router state
 
   if (!post) return <div>Loading...</div>;
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">{post.heading}</h1>
+      <h1 className="text-2xl font-bold mb-4">{post ? post.heading : 'Post not found'}</h1>
       {/* Additional content and layout for the post details */}
     </div>
   );
